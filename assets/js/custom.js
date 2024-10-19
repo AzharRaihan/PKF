82627fbdfc40c630654999b8f4a74042b3520514
AzharRaihan <azharraihan6969@gmail.con>
2024-10-19 15:17:58 +0600

    pdf making and php mailer integrate done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,8 +3,20 @@ $(function(){
 
 
     $('.bar-trigger').click(function(){
-      $('#mySidenav').css('width', '450px');
+      if ($('#MainNav').hasClass('show')) {
+        $('#MainNav').removeClass('show');
+      } else {
+        $('#MainNav').addClass('show');
+      }
     });
+    $('.bar-trigger-2').click(function(){
+      if ($('#MainNav').hasClass('show')) {
+        $('#MainNav').removeClass('show');
+      } else {
+        $('#MainNav').addClass('show');
+      }
+    });
+    
     $('.closebtn').click(function(){
       $('#mySidenav').css('width', '0px');
     });
@@ -15,15 +27,30 @@ $(function(){
       $('#mySearchbar').show();
       $('#mySearchbar').css('width', '450px');
     });
+    $('.search-trigger-2').click(function(){
+      $(this).attr('search-close', true);
+
+      $('#mySearchbar-2').show();
+      $('#mySearchbar-2').css('width', '350px');
+    });
+
+    $('#mySearchbar-2').hide();
+
 
 
     // Get the elements
     const searchTrigger = document.querySelector('.search-trigger');
     const searchbar = document.getElementById('mySearchbar');
+    const searchTrigger2 = document.querySelector('.search-trigger-2');
+    const searchbar2 = document.getElementById('mySearchbar-2');
 
     // Function to open the search bar
     searchTrigger.addEventListener('click', function() {
         searchbar.style.display = 'block'; // Show the search bar
+    });
+    // Function to open the search bar
+    searchTrigger2.addEventListener('click', function() {
+      searchbar.style.display = 'block'; // Show the search bar
     });
 
     // Close the search bar when clicking outside of it
@@ -31,6 +58,9 @@ $(function(){
         // Check if the clicked target is not the search bar or the bar trigger
         if (!searchbar.contains(event.target) && !searchTrigger.contains(event.target)) {
             searchbar.style.display = 'none'; // Hide the search bar
+        }
+        if (!searchbar2.contains(event.target) && !searchTrigger2.contains(event.target)) {
+            searchbar2.style.display = 'none'; // Hide the search bar
         }
     });
 });
